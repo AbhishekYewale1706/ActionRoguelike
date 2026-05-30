a09f19d1817ccf6bd561df526cb8661873070804
Abhishek Yewale <1706abhishekyewale@gmail.com>
2026-05-30 11:47:43 +0530

    Lecture 1 : Character Setup

---
+++ Source/ActionRoguelike/Core/RogueCharacter.cpp
@@ -0,0 +1,61 @@
+﻿#include "RogueCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ARogueCharacter::ARogueCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	//Camera
+	SpringArmComponent=CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->bUsePawnControlRotation=true;
+	
+	CameraComponent=CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	
+}
+
+void ARogueCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+
+void ARogueCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARogueCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* Input=Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	Input->BindAction(InputMove,ETriggerEvent::Triggered,this,&ARogueCharacter::Move);
+	Input->BindAction(InputLook,ETriggerEvent::Triggered,this,&ARogueCharacter::Look);
+	
+}
+
+void ARogueCharacter::Move(const FInputActionValue& InputValue)
+{
+	FVector2D Value=InputValue.Get<FVector2D>();
+	
+	//Froward/Backward Direction
+	FRotator ControllerRotation=GetControlRotation();
+	ControllerRotation.Pitch=0;
+	ControllerRotation.Roll=0;
+	AddMovementInput(ControllerRotation.Vector(),Value.X);
+	
+	//Right/Left Direction
+	FVector RightDirection=GetActorRightVector();
+	AddMovementInput(RightDirection,Value.Y);
+}
+
+void ARogueCharacter::Look(const FInputActionValue& InputValue)
+{
+    FVector2D Value=InputValue.Get<FVector2D>();
+	AddControllerYawInput(Value.X);
+	AddControllerPitchInput(Value.Y);
+}
---
+++ Source/ActionRoguelike/Core/RogueCharacter.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "RogueCharacter.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ARogueCharacter();
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly,Category="Input")
+	TObjectPtr<UInputAction> InputMove;
+	
+	UPROPERTY(EditDefaultsOnly,Category="Input")
+	TObjectPtr<UInputAction> InputLook;
+	
+	UPROPERTY(VisibleAnywhere,Category="Camera")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+	UPROPERTY(VisibleAnywhere,Category="Camera")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	
+	virtual void BeginPlay() override;
+
+	void Move(const FInputActionValue& InputValue);
+	void Look(const FInputActionValue& InputValue);
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
